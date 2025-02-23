bb084783972af69d17ce33c43c687e31c0f9809b
Ophir Bucai <ophirbucai@gmail.com>
2025-02-23 21:48:03 +0200

    feat: handle blur event in name area

--- src/client/components/profile-name-area.tsx
+++ src/client/components/profile-name-area.tsx
@@ -2,15 +2,13 @@ import { useCallback, useEffect, useRef, useState } from "react";
 import { useUser } from "../providers/user.provider";
 
 export const ProfileNameArea = () => {
-	const { user, renameUser } = useUser((state) => state);
-	const [loading, setLoading] = useState<boolean>(false);
+	const { user, renameUser, refetchName } = useUser((state) => state);
 	const [name, setName] = useState(user?.name);
 
 	const submitName = useCallback(
 		(e: React.FormEvent<HTMLFormElement>) => {
 			e.preventDefault();
 			if (e.target instanceof HTMLFormElement) {
-				setLoading(true);
 				(document.activeElement as HTMLElement)?.blur();
 				const name = new FormData(e.target).get("name") as string;
 				renameUser(name);
@@ -21,8 +19,7 @@ export const ProfileNameArea = () => {
 
 	useEffect(() => {
 		setName(user?.name);
-		setLoading(false);
-	}, [user?.name]);
+	}, [user]);
 
 	return (
 		<form onSubmit={submitName} className="relative">
@@ -30,11 +27,12 @@ export const ProfileNameArea = () => {
 				placeholder="What is your name?"
 				value={name ?? ""}
 				onChange={(e) => setName(e.target.value)}
-				onBlur={!loading ? () => setName(user?.name) : undefined}
+				onBlur={() => refetchName()}
 				aria-label="Your name"
 				name="name"
 				className="bg-white/50 px-1.5 py-0.5 rounded-sm shadow-sm placeholder:text-slate-400 text-sm focus:outline-solid focus:outline-blue-300/50 focus:outline-3 w-52 focus:pr-6 peer"
 				required
+				autoComplete="off"
 			/>
 			<button
 				type="submit"
